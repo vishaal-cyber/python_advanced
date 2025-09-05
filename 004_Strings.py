a7ea882aed782177e164d34d1513277fdc9e00c9
Ramakant Debata <ramakant.s.debata@gmail.com>
2025-09-05 10:56:53 +0530

    Day 03

--- 004_Strings.py
+++ 004_Strings.py
@@ -36,3 +36,63 @@ print(s8)
 ## Escaape Sequence
 ## Format Strings
 ## Slicing
+
+path = r"c:\Users\ramakant\new_folder\textpad.txt"
+print(path)
+
+# printf("%d is te count of %s type of items", count, category)
+
+s1 = "We are in a %s training for %d days"
+print(s1%('Python', 12))
+
+s1 = "We are in a {} training for {} days"
+print(s1.format('Python', 12))
+
+tech = 'Python'
+duration = 12
+print(f"We are in a {tech} training for {duration} days")
+
+print("tech =", tech)
+print(f"{tech = }")
+
+
+## String Slicing
+s2 = f"We are in a {tech} training for {duration} days"
+print(s2)
+
+for idx in range(3, 6, 1):
+    print(s2[idx], end='')
+print()
+
+print(s2[3:6])
+print(s2[3:])
+print(s2[:6])
+
+print(s2[len(s2) - 1])
+print(s2[-1])
+print(s2[-4:])
+
+print(s2[-11:-5])
+
+
+# We are in a Python training for 12 days
+# days We are --> s2[-5:6], s2[-4:6], s2[-4:]+s2[:6]
+print("="*25)
+print(s2[-4:6])
+print(s2[-4:]+s2[:6])
+
+
+
+s3 = "0123456789"
+
+
+# s3[start : stop : step]
+print(s3[::2])
+print(s3[1::2])
+print(s3[::-1])
+print(s2[-13:-21:-1])
+
+print("="*25)
+print(s2[3:9:-1])
+print(s2[19:27:1])
+print(s2[26:18:-1])
--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 * [Pep Index](https://peps.python.org/#)
 * [Style Guide for Python](https://peps.python.org/pep-0008/)
 * [Zen of Python](https://peps.python.org/pep-0020/)
+* [Lexical Analysis](https://docs.python.org/3.11/reference/lexical_analysis.html#lexical-analysis)
+    * [Escape Sequences](https://docs.python.org/3.11/reference/lexical_analysis.html#escape-sequences)
+    * [Format Specifications](https://docs.python.org/3.11/library/string.html#format-specification-mini-language)
+    
