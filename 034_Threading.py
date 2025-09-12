1a4e7fe4be83853148cf94b9eaf04e50b1d7d66f
Ramakant Debata <ramakant.s.debata@gmail.com>
2025-09-12 13:14:37 +0530

    Day 08

--- 034_Threading.py
+++ 034_Threading.py
@@ -2,27 +2,31 @@ import threading
 
 
 
-def Foo(coll: list[int], res) -> int:
-    print(f"{id(res) = } in {threading.current_thread().name}")
-    res =  sum(coll)
+def Foo(coll: list[int], res_lst) -> int:
+    # print(f"{id(res) = } in {threading.current_thread().name}")
+    print(f"{coll = } in {threading.current_thread().name}")
+    res_lst.append(sum(coll))
+    # print(f"{id(res) = } in {threading.current_thread().name}")
 
 
 def Main(data: list[int]) -> None:
     batch_size = 25
-    res1 = 0
-    res2 = 0
-    res3 = 0
-    res4 = 0
+    # res1 = 0
+    # res2 = 1
+    # res3 = 2
+    # res4 = 3
 
-    print(f"{id(res1) = }")
-    print(f"{id(res2) = }")
-    print(f"{id(res3) = }")
-    print(f"{id(res4) = }")
+    # print(f"{id(res1) = }")
+    # print(f"{id(res2) = }")
+    # print(f"{id(res3) = }")
+    # print(f"{id(res4) = }")
 
-    t1 = threading.Thread(target=Foo, name="Thread_1", args=(lst[0:25], res1))
-    t2 = threading.Thread(target=Foo, name="Thread_2", args=(lst[25:50], res2))
-    t3 = threading.Thread(target=Foo, name="Thread_3", args=(lst[50:75], res3))
-    t4 = threading.Thread(target=Foo, name="Thread_4", args=(lst[75:100], res4))
+    lst_res = []
+
+    t1 = threading.Thread(target=Foo, name="Thread_1", args=(lst[0:25], lst_res))
+    t2 = threading.Thread(target=Foo, name="Thread_2", args=(lst[25:50], lst_res))
+    t3 = threading.Thread(target=Foo, name="Thread_3", args=(lst[50:75], lst_res))
+    t4 = threading.Thread(target=Foo, name="Thread_4", args=(lst[75:100], lst_res))
 
     t1.start()
     t2.start()
@@ -34,11 +38,30 @@ def Main(data: list[int]) -> None:
     t3.join()
     t4.join()
 
-    print(f"{res1}, {res2}, {res3}, {res4}")
-    final = res1 + res2 + res3 + res4
+    # print(f"{id(res1) = }")
+    # print(f"{id(res2) = }")
+    # print(f"{id(res3) = }")
+    # print(f"{id(res4) = }")
+
+
+    # print(f"{res1}, {res2}, {res3}, {res4}")
+    # final = res1 + res2 + res3 + res4
+
+    print(f"{lst_res = }")
+    final = sum(lst_res)
     print(f"Result --> {final}")
-    print(final == sum(lst))
+    print(final == sum(data))
 
 if __name__ == "__main__":
     lst = [x for x in range(100)]
     Main(lst)
+
+## Thread synchronisation objects
+#       * threading.Lock()
+#       * threading.RLock()
+#       * threading.Semaphore()
+#       * threading.BoundedSemaphore()
+#       * threading.Condition()
+#       * threading.Event()
+#       * import queue.Queue()
+
