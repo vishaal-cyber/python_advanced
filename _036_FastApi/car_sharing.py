a4b2326de4123547e8052b825da5165f5a0e60e6
Ramakant Debata <ramakant.s.debata@gmail.com>
2025-09-15 16:35:54 +0530

    Day 09

--- _036_FastApi/car_sharing.py
+++ _036_FastApi/car_sharing.py
@@ -4,21 +4,12 @@ from fastapi import HTTPException
 from fastapi.responses  import FileResponse
 # from typing import Optional
 from schemas import Car
+from schemas import load_lib
 import uvicorn
 
 import os
 
-db = [
-    {"id": 1, "size": "s", f"fuel": "petrol",   "door": 3, "transmission": "auto"},
-    {"id": 2, "size": "s", f"fuel": "electric", "door": 3, "transmission": "auto"},
-    {"id": 3, "size": "s", f"fuel": "petrol",   "door": 5, "transmission": "manual"},
-    {"id": 4, "size": "m", f"fuel": "electric", "door": 3, "transmission": "auto"},
-    {"id": 5, "size": "m", f"fuel": "hybrid",   "door": 5, "transmission": "auto"},
-    {"id": 6, "size": "m", f"fuel": "petrol",   "door": 4, "transmission": "manual"},
-    {"id": 7, "size": "l", f"fuel": "disel",    "door": 4, "transmission": "manual"},
-    {"id": 8, "size": "l", f"fuel": "petrol",   "door": 5, "transmission": "auto"},
-    {"id": 9, "size": "l", f"fuel": "hybrid",   "door": 5, "transmission": "auto"}
-]
+db = load_lib()
 
 app = FastAPI()
 
@@ -40,36 +31,19 @@ def favicon():
     """Serves the favicon.ico file"""
     return FileResponse(os.path.join(os.path.dirname(__file__), 'static', 'sun.png'))
 
-# @app.get("/api/cars")
-# def get_cars():
-#     """Returns a collection of cars from the server records."""
-#     return db
-
 
 ## Query Params     (http://localhost:8000/api/cars?size=s&doors=4)
 # Filter the data based on 'size' and/or 'doors'
 @app.get("/api/cars")
-def get_cars(size:str|None=None, doors:int|None=None):
-# def get_cars(size:Optional[str]=None, doors:Optional[int]=None):
+def get_cars(size:str|None=None, doors:int|None=None) -> list[Car]:
     """Returns a collection of cars from the server records."""
-    # if not isinstance(int(doors), int):
-    #     raise TypeError("doors should be an integer")
-    # # if size:
-    #     return [car for car in db if car['size'] == size]
-    # else:
-    #     return db
-    # print(type(size), size, type(doors), doors)
-
-    # return [car for car in db if car['size'] == size and car['door'] >= int(doors)]
-    # return [car for car in db if car['size'] == size and car['door'] >= doors]
-
     results = db
 
     if size:
-        results =  [car for car in results if car['size'] == size]
+        results =  [car for car in results if car.size == size]
 
     if doors:
-        results =  [car for car in results if car['door'] >= doors]
+        results =  [car for car in results if car.doors >= doors]
 
     return results
 
@@ -78,11 +52,13 @@ def get_cars(size:str|None=None, doors:int|None=None):
 # Retrieve a specific car by id
 @app.get("/api/cars/{id}")
 def car_by_id(id:int):
-    results = [car for car in db if car['id'] == id]
+    """Retrieve a specific ar by its id"""
+    # results = [car for car in db if car['id'] == id]
+    results = [car for car in db if car.id == id]
     if results:
-        return results[0]
+        return Car.model_validate(results[0])
     else:
         raise HTTPException(status_code=404, detail=f"No car with id={id}")
 
 if __name__ == "__main__":
-    uvicorn.run("car_sharing:app", reload=True)
+    uvicorn.run("car_sharing:app")#, reload=True)
--- _036_FastApi/schemas.py
+++ _036_FastApi/schemas.py
@@ -5,10 +5,21 @@
     # __repr__
 
 from pydantic import BaseModel
+import json
+import os
 
 class Car(BaseModel):
+    """Model for Car class"""
     id: int
     size: str
     fuel: str|None = "electric"
     doors: int
     transmission: str|None = "auto"
+
+fileName = "cars.json"
+filePath = os.path.join(os.path.dirname(__file__), fileName)
+
+def load_lib():
+    """Loads a list of Car data from a JSON file"""
+    with open(filePath) as f:
+        return [Car.model_validate(obj)  for obj in json.load(f)]
